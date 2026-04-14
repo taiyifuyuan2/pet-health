6cbbc6bbd14562179618725f4c58f66aac81cb81
taichi.fukumoto <taichi.fukumoto@leadingsolution.co.jp>
2026-04-14 15:35:25 +0900

    fix: remove vite-plugin-pwa for vite 8 compat

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icon-192.png', 'icon-512.png'],
-      manifest: {
-        name: 'ペット健康管理',
-        short_name: 'PetHealth',
-        description: 'ラムちゃん・モカちゃんの健康管理アプリ',
-        theme_color: '#08080d',
-        background_color: '#08080d',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
-        ],
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      },
-    }),
-  ],
+  plugins: [react()],
 })
